44a2c595f0141f2860a6294f0d086f3a2d0a7064
ekstroem <github@ekstroem.com>
2017-07-02 03:47:03 +0200

    Buildable

---
+++ R/Austin-package.R
@@ -0,0 +1,21 @@
+#' Collection of miscellaneous useful and semi-useful functions
+#'
+#' Collection of miscellaneous useful and semi-useful functions and add-on
+#' functions that enhances a number of existing packages and provides In
+#' particular in relation to statistical genetics
+#'
+#' \tabular{ll}{ Package: \tab Austin\cr Type: \tab Package\cr Version: \tab
+#' 1.0\cr Date: \tab 2012-03-29\cr License: \tab GPL-2\cr } % ~~ An overview of
+#' how to use the package, including the most important ~~ % ~~ functions ~~
+#'
+#' @name Austin
+#' @aliases Austin-package Austin
+#' @docType package
+#' @author Claus Thorn Ekstrøm \email{ekstrom@@sund.ku.dk}\cr Maintainer: Claus Thorn Ekstrøm
+#' \email{ekstrom@@sund.ku.dk}
+#' @references Ekstrøm, C. (2017). The R Primer. Chapman & Hall, 2nd edition.
+#' @import utils stats graphics
+#' @keywords package
+#' # '   @ u s eDy n L ib Austin, .registration = TRUE
+#' # ' @ i  mp o r t  From Rcpp sourceCpp
+NULL
