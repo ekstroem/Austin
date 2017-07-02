91691559b62f6534808f59e4bbfec2b5e336ae33
ekstroem <github@ekstroem.com>
2017-07-02 11:11:11 +0200

    Fixed documentation

--- R/Austin-package.R
+++ R/Austin-package.R
@@ -7,11 +7,8 @@
 #' @name Austin
 #' @aliases Austin-package Austin
 #' @docType package
-#' @author Claus Thorn Ekstrøm \email{ekstrom@@sund.ku.dk}\cr Maintainer: Claus Thorn Ekstrøm
-#' \email{ekstrom@@sund.ku.dk}
+#' @author Claus Thorn Ekstrøm \email{ekstrom@@sund.ku.dk}\cr Maintainer: Claus Thorn Ekstrøm \email{ekstrom@@sund.ku.dk}
 #' @references Ekstrøm, C. (2017). The R Primer. Chapman & Hall, 2nd edition.
 #' @import utils stats graphics
 #' @keywords package
-#' # '   @ u s eDy n L ib Austin, .registration = TRUE
-#' # ' @ i  mp o r t  From Rcpp sourceCpp
 NULL
